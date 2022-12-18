e1789c078e0e1f3f9fb6aa61bcfc19c47b73de2e
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2022-12-18 02:41:22 +0100

    Fix typo 'convec' to 'convex'

--- src/algorithms/graham_scan.rs
+++ src/algorithms/graham_scan.rs
@@ -15,51 +15,51 @@ impl GrahamScan {
         // Sort points lexicographically.
         points.sort_by(|a, b| (a.x, a.y).partial_cmp(&(b.x, b.y)).unwrap());
 
-        let mut upper_convec_hull: Vec<Point2<f64>> = vec![];
+        let mut upper_convex_hull: Vec<Point2<f64>> = vec![];
 
         // Insert the first two points.
-        upper_convec_hull.push(points.remove(0));
-        upper_convec_hull.push(points.remove(0));
+        upper_convex_hull.push(points.remove(0));
+        upper_convex_hull.push(points.remove(0));
 
         for _ in 0..points.len() {
-            upper_convec_hull.push(points.remove(0));
-            while upper_convec_hull.len() > 2
+            upper_convex_hull.push(points.remove(0));
+            while upper_convex_hull.len() > 2
                 && GrahamScan::cross_product(
-                    &upper_convec_hull[upper_convec_hull.len() - 3],
-                    &upper_convec_hull[upper_convec_hull.len() - 2],
-                    &upper_convec_hull[upper_convec_hull.len() - 1],
+                    &upper_convex_hull[upper_convex_hull.len() - 3],
+                    &upper_convex_hull[upper_convex_hull.len() - 2],
+                    &upper_convex_hull[upper_convex_hull.len() - 1],
                 ) > 0.0
             {
-                upper_convec_hull.remove(upper_convec_hull.len() - 2);
+                upper_convex_hull.remove(upper_convex_hull.len() - 2);
             }
         }
 
-        upper_convec_hull
+        upper_convex_hull
     }
 
     pub fn scan_lower(points: &mut Vec<Point2<f64>>) -> Vec<Point2<f64>> {
         // Sort points lexicographically.
         points.sort_by(|a, b| (a.x, a.y).partial_cmp(&(b.x, b.y)).unwrap());
 
-        let mut lower_convec_hull: Vec<Point2<f64>> = vec![];
+        let mut lower_convex_hull: Vec<Point2<f64>> = vec![];
 
         // Insert the first two points.
-        lower_convec_hull.push(points.remove(0));
-        lower_convec_hull.push(points.remove(0));
+        lower_convex_hull.push(points.remove(0));
+        lower_convex_hull.push(points.remove(0));
 
         for _ in 0..points.len() {
-            lower_convec_hull.push(points.remove(0));
-            while lower_convec_hull.len() > 2
+            lower_convex_hull.push(points.remove(0));
+            while lower_convex_hull.len() > 2
                 && GrahamScan::cross_product(
-                    &lower_convec_hull[lower_convec_hull.len() - 3],
-                    &lower_convec_hull[lower_convec_hull.len() - 2],
-                    &lower_convec_hull[lower_convec_hull.len() - 1],
+                    &lower_convex_hull[lower_convex_hull.len() - 3],
+                    &lower_convex_hull[lower_convex_hull.len() - 2],
+                    &lower_convex_hull[lower_convex_hull.len() - 1],
                 ) < 0.0
             {
-                lower_convec_hull.remove(lower_convec_hull.len() - 2);
+                lower_convex_hull.remove(lower_convex_hull.len() - 2);
             }
         }
 
-        lower_convec_hull
+        lower_convex_hull
     }
 }
