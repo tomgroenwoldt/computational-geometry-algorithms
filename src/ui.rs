810f89428ee3e59e744aa8c2f7002a96cf3d0e32
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2022-12-28 02:12:43 +0100

    Add progress bar to ui

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use tui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
-    widgets::{Block, Borders, Paragraph, Tabs, Wrap},
+    widgets::{Block, Borders, Gauge, Paragraph, Tabs, Wrap},
     Frame,
 };
 
@@ -67,14 +67,37 @@ where
     f.render_widget(paragraph, area);
 }
 
+fn draw_progress<B>(f: &mut Frame<B>, area: Rect, app: &App)
+where
+    B: Backend,
+{
+    let mut ratio = 0.0;
+    if app.get_current_tab().algorithm.get_maximum_step_count() > 0 {
+        ratio = app.get_current_tab().step as f64
+            / (app.get_current_tab().algorithm.get_maximum_step_count() - 1) as f64;
+    }
+    let label = format!("{:.2}%", ratio * 100.0);
+    let gauge = Gauge::default()
+        .block(Block::default().borders(Borders::ALL).title("Progress"))
+        .gauge_style(Style::default().fg(Color::Green).bg(Color::Black))
+        .label(label)
+        .ratio(ratio);
+    f.render_widget(gauge, area);
+}
+
 fn draw_footer<B>(f: &mut Frame<B>, area: Rect, app: &App, _tab: &Tab)
 where
     B: Backend,
 {
-    let chunks = Layout::default()
+    let vertical_chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Length(1), Constraint::Length(3)].as_ref())
         .split(area);
+
+    let horizontal_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(vertical_chunks[1]);
 
     let block = Block::default()
         .borders(Borders::ALL)
@@ -106,7 +129,7 @@ where
     let mut text = Text::from(Spans::from(msg));
     text.patch_style(style);
     let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[0]);
+    f.render_widget(help_message, vertical_chunks[0]);
 
     let input = Paragraph::new(app.input.as_ref())
         .style(match app.input_mode {
@@ -114,12 +137,15 @@ where
             InputMode::Editing => Style::default().fg(Color::Yellow),
         })
         .block(block);
-    f.render_widget(input, chunks[1]);
+    f.render_widget(input, horizontal_chunks[0]);
 
     match app.input_mode {
         InputMode::Normal => {}
-        InputMode::Editing => {
-            f.set_cursor(chunks[1].x + app.input.len() as u16 + 1, chunks[1].y + 1)
-        }
+        InputMode::Editing => f.set_cursor(
+            horizontal_chunks[0].x + app.input.len() as u16 + 1,
+            horizontal_chunks[0].y + 1,
+        ),
     }
+
+    draw_progress(f, horizontal_chunks[1], app);
 }
